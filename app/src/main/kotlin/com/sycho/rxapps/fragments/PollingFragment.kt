4d8a151c076467f5ef4f644862d7659b77e33c83
SeongyongCho <lilisleaves@gmail.com>
2019-03-27 16:43:40 +0900

    Changing Java Timer to repeatWhen()

--- app/src/main/kotlin/com/sycho/rxapps/fragments/PollingFragment.kt
+++ app/src/main/kotlin/com/sycho/rxapps/fragments/PollingFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import com.sycho.rxapps.R
 import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.fragment_polling.*
 import java.util.concurrent.TimeUnit
@@ -21,6 +22,8 @@ class PollingFragment : BaseFragment() {
         private const val PERIOD: Long = 3L
     }
 
+    private var disposables: CompositeDisposable? = null
+
     override fun getLayoutId(): Int {
         return R.layout.fragment_polling
     }
@@ -32,13 +35,20 @@ class PollingFragment : BaseFragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
+        disposables = CompositeDisposable()
+
         btn_polling.setOnClickListener {
             startPollingV1()
         }
 
         btn_polling2.setOnClickListener {
-
+            startPollingV2()
         }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        disposables?.clear()
     }
 
     /**
@@ -54,8 +64,25 @@ class PollingFragment : BaseFragment() {
             }
 
         // 구독하는곳에서는 로그 리스트에 추가해주는걸로 끝이다.
-        ob.subscribeOn(Schedulers.io())
+        val disposable = ob.subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe(this::log)
+
+        disposables?.add(disposable)
+    }
+
+    private fun startPollingV2() {
+        // 다른 방법으로 구현한 예이다.
+        // Observable 생성은 just로 한번만 하고
+        // 이 Observable을 repeatWhen()으로 계속 발행하게 한다.
+        // delay를 3초로 줬으므로 3초마다 Observable을 계속해서 발행하게 된다
+        val ob2 = Observable.just("polling #2")
+            .repeatWhen { o -> o.delay(PERIOD, TimeUnit.SECONDS) }
+
+        val disposable = ob2.subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(this::log)
+
+        disposables?.add(disposable)
     }
 }
